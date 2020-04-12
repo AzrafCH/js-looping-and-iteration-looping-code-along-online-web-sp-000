476467ac0fba4cd5b180405026f8a2d83690b98e
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 04:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  const cards = [];
+const cards = [];
 
-  for(let i = 0; i < cards.length; i++) {
-    cards.push('Thank you, ${names[i]}, for the wonderful ${event} gift!');
-  }
-  return cards;
+for(let i = 0; i < names.length; i++) {
+  cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+
+return cards;
 }
